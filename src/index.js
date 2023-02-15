d09f7b67ab2bc56ad4338b3e0f7a31c25a179cc9
kicki228 <118886828+kicki228@users.noreply.github.com>
2023-02-15 12:38:53 +0100

    startade websocket

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
+const { io } = require("socket.io-client");
+const socket = io();
+console.log(io);
+
 //sfdsdsdfsdf
 
 //sfasfdsfsdfsdf
