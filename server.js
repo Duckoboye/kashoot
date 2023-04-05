15f299fae8c78ad5765431a04641e7f107f6c351
Duckoboye <48358179+Duckoboye@users.noreply.github.com>
2023-04-05 10:42:17 +0200

    THE BIRTH OF THE HOLY MONOREPO

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'client/build')));  //  
+app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
+
+//build mode
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
+
+//Route setup
+app.get('/', (req, res) => {
+    res.send('root route')
+});
+
+////socket io shit
+io.on('connection', (socket) => {
+	console.log('a user connected');
+	socket.on('disconnect', () => {
+		console.log('a user disconnected');
+		
+	
+	})
+	socket.on('chat message', (msg) => {
+		console.log('message: ' + msg);
+		});
+  });
+
+
+//start server
+server.listen(port,() => {
+    console.log(`server listening on http://127.0.0.1:${port}`);
+});
