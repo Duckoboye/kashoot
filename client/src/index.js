269e7609b5b6e703e6526192e38c7b135591613e
Duckoboye <swedishshadows@gmail.com>
2023-04-15 13:13:16 +0200

    add auth0provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,30 @@
-import React from "react";
-import App from "./pages/App";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import App from './pages/App';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 export default function go() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<Auth0Provider
+		domain="dev-pyzrhlrc.us.auth0.com"
+		clientId="OCNZ2wePKUZvWYJ6X6F2S8p8AkdAsTD8"
+		authorizationParams={{
+			redirect_uri: window.location.origin,
+		}}
+	>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Auth0Provider>
 );
