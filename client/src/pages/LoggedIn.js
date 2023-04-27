c81ea7dfad9142552053198e34b7234329d7cf65
kicki228 <118886828+kicki228@users.noreply.github.com>
2023-04-27 13:35:15 +0200

    socket.io fix

--- client/src/pages/LoggedIn.js
+++ client/src/pages/LoggedIn.js
@@ -2,13 +2,11 @@ import "../index.css";
 import logo from "../pic/logo.svg";
 import React, { useState, useEffect } from "react";
 import { socket } from "../socket";
-import { ConnectionState } from "../components/ConnectionState";
 import { ConnectionManager } from "../components/ConnectionManager";
 import { MyForm } from "../components/MyForm";
 
 function LoggedIn() {
   const [isConnected, setIsConnected] = useState(socket.connected);
-  const [fooEvents, setFooEvents] = useState([]);
 
   useEffect(() => {
     function onConnect() {
@@ -19,17 +17,11 @@ function LoggedIn() {
       setIsConnected(false);
     }
 
-    function onFooEvent(value) {
-      setFooEvents((previous) => [...previous, value]);
-    }
-
     socket.on("connect", onConnect);
     socket.on("disconnect", onDisconnect);
-    socket.on("foo", onFooEvent);
     return () => {
       socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-      socket.off("foo", onFooEvent);
+      //socket.off("disconnect", onDisconnect);
     };
   }, []);
 
