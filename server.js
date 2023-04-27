9964a82e97dc5c5fdc53a155406cb28118d78cc5
kicki228 <118886828+kicki228@users.noreply.github.com>
2023-04-27 14:09:20 +0200

    changed " to ' in server.js

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
+const path = require('path');
 const port = process.env.PORT || 5000;
 
-const http = require("http");
+const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 const io = new Server(server);
 
 //Static file declaration
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 //production mode
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build"))); //
-  app.get("*", (req, res) => {
-    res.sendfile(path.join((__dirname = "client/build/index.html")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build'))); //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join((__dirname = 'client/build/index.html')));
   });
 }
 
 //build mode
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'));
 });
 
 //Route setup
-app.get("/", (req, res) => {
-  res.send("root route");
+app.get('/', (req, res) => {
+  res.send('root route');
 });
 
 ////socket io shit
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("a user disconnected");
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
   });
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
   });
 });
 
