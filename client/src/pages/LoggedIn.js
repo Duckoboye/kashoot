c067e85e862a63b9f913c4d90f446cf3722f8702
kicki228 <118886828+kicki228@users.noreply.github.com>
2023-04-27 13:25:30 +0200

    socket io now works

--- client/src/pages/LoggedIn.js
+++ client/src/pages/LoggedIn.js
@@ -1,33 +1,55 @@
 import "../index.css";
 import logo from "../pic/logo.svg";
+import React, { useState, useEffect } from "react";
+import { socket } from "../socket";
+import { ConnectionState } from "../components/ConnectionState";
+import { ConnectionManager } from "../components/ConnectionManager";
+import { MyForm } from "../components/MyForm";
 
 function LoggedIn() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value) {
+      setFooEvents((previous) => [...previous, value]);
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("foo", onFooEvent);
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("foo", onFooEvent);
+    };
+  }, []);
+
   return (
     <div className="LoggedIn">
       <header>
         <img className="logo" src={logo} alt="logo" />
       </header>
-
       <h2 className="join_text"> Join a kashoot</h2>
-
       <div className="form">
-        <form
-          id="register-form"
-          method="post"
-          action="http://192.168.72.45:8080"
-        >
-          <input type="text" placeholder="name" classname="name"></input>
+        <form>
+          <input type="text" placeholder="name" className="name"></input>
           <br></br>
-          <input
-            type="code"
-            required
-            placeholder="1234"
-            classname="code"
-          ></input>
           <br></br>
-          <input type="submit" value="Join!" classname="submit"></input>
+          <input type="submit" value="Join!" className="submit"></input>
         </form>
       </div>
+      <p>State: {"" + isConnected}</p>
+      <ConnectionManager />
+      <MyForm />
     </div>
   );
 }
