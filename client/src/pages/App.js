a21473caeacc1744c0ed05df092744de203d46cd
Duckoboye <48358179+Duckoboye@users.noreply.github.com>
2023-05-10 09:47:03 +0200

    Loggedin/Login logic

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,19 +1,19 @@
 import "../index.css";
 import logo from "../pic/logo.svg";
 import LoginButton from "../components/LoginButton";
+import Login from "./Login";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoggedIn from "./LoggedIn";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header>
-        <img className="start_logo" src={logo} alt="logo" />
-      </header>
-      <div className="knappar">
-        <LoginButton></LoginButton>
-      </div>
-      <a href="LoggedIn">eheh </a>
-    </div>
-  );
+  
+
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+  return(isAuthenticated?<LoggedIn/>:<Login/>)
 }
 
 export default App;
