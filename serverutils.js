f44f6078b1f8a58bde2e6476b34339be9827e2b6
Duckoboye <48358179+Duckoboye@users.noreply.github.com>
2023-08-31 08:45:47 +0200

    Server logging system

---
+++ serverutils.js
@@ -0,0 +1,36 @@
+//Handles logging. Please refactor me!
+generic = {
+    express: (text) => {
+        console.log(`\x1b[33m[EXPRESS]\x1b[0m ${text}`)
+    },
+    socketio: (text) => {
+        console.log(`\x1b[37m[SocketIO]\x1b[0m ${text}`)
+    }
+}
+log = {
+    express: (text) => {
+        generic.express(`\x1b[92mLOG |\x1b[0m ${text}`)
+    },
+    socketio: (text) => {
+        generic.socketio(`\x1b[92mLOG |\x1b[0m ${text}`)
+    }
+}
+warn = {
+    express: (text) => {
+        generic.express(`\x1b[93mWARN |\x1b[0m ${text}`)
+    },
+    socketio: (text) => {
+        generic.socketio(`\x1b[93mWARN |\x1b[0m ${text}`)
+    }
+}
+error = {
+    express: (text) => {
+        generic.express(`\x1b[91mERROR |\x1b[0m ${text}`)
+    },
+    socketio: (text) => {
+        generic.socketio(`\x1b[91mERROR |\x1b[0m ${text}`)
+    }
+}
+
+
+module.exports = { log, warn, error }
