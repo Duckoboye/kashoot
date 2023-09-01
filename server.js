4e613762e9c556e3fd7e6a7b423a2730c324f192
Duckoboye <48358179+Duckoboye@users.noreply.github.com>
2023-09-01 13:04:18 +0200

    MVP kashoot system

--- server.js
+++ server.js
@@ -18,7 +18,19 @@ io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     utils.log.socketio('message: ' + msg);
   });
+  socket.on('GameStartReq', () => {
+    utils.log.socketio('Received GameStartReq, broadcasting GameStarting event to all clients.')
+    socket.broadcast.emit('GameStarting', 'wow sample text, this will send the amount of players and some other fun info later')
+    setTimeout(() => {
+      utils.log.socketio('timeout elapsed, sending first Question..')
+      socket.broadcast.emit("GameQuestion", {"question": "Bla bla bla, test", "answers": [{"id": 1, "answer": "bla bla svar 1"}, {"id": 2, "answer": "bla bla svar 2"}, {"id": 3, "answer": "bla bla svar 3"}, {"id": 4, "answer": "blas ba svar 4"}]})
+    }, 5000)
+  })
 });
+
+app.get('/', (req, res) => {
+  res.send('API')
+})
 
 
 server.listen(utils.config.port, () => {
