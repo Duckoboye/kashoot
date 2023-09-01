5529273144e91a54365b7580f19b0f776229d776
Duckoboye <48358179+Duckoboye@users.noreply.github.com>
2023-09-01 13:10:32 +0200

    moved socket logic to own file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const utils = require('./serverUtils')
-
+const socket = require('./socket')
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
@@ -10,28 +10,11 @@ const io = new Server(server, utils.config.cors);
 
 app.use(cors())
 
-io.on('connection', (socket) => {
-  utils.log.socketio('a user connected');
-  socket.on('disconnect', () => {
-    utils.log.socketio('a user disconnected');
-  });
-  socket.on('chat message', (msg) => {
-    utils.log.socketio('message: ' + msg);
-  });
-  socket.on('GameStartReq', () => {
-    utils.log.socketio('Received GameStartReq, broadcasting GameStarting event to all clients.')
-    socket.broadcast.emit('GameStarting', 'wow sample text, this will send the amount of players and some other fun info later')
-    setTimeout(() => {
-      utils.log.socketio('timeout elapsed, sending first Question..')
-      socket.broadcast.emit("GameQuestion", {"question": "Bla bla bla, test", "answers": [{"id": 1, "answer": "bla bla svar 1"}, {"id": 2, "answer": "bla bla svar 2"}, {"id": 3, "answer": "bla bla svar 3"}, {"id": 4, "answer": "blas ba svar 4"}]})
-    }, 5000)
-  })
-});
+io.on('connection', socket);
 
 app.get('/', (req, res) => {
   res.send('API')
 })
-
 
 server.listen(utils.config.port, () => {
   utils.log.express(`API server listening on http://127.0.0.1:${utils.config.port}`);
