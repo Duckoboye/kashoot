fab2f6365308e2c0b2b1e8a1c68fe60945906c43
Duckoboye <48358179+Duckoboye@users.noreply.github.com>
2023-09-07 09:54:14 +0200

    Quick fix to make broadcasts work again (I hope)

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors')
 const utils = require('./serverUtils')
-const socket = require('./socket')
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
@@ -10,7 +9,23 @@ const io = new Server(server, utils.config.cors);
 
 app.use(cors())
 
-io.on('connection', socket);
+io.on('connection', (socket) => {
+  utils.log.socketio('a user connected');
+    socket.on('disconnect', () => {
+      utils.log.socketio('a user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+      utils.log.socketio('message: ' + msg);
+    });
+    socket.on('GameStartReq', () => {
+      utils.log.socketio('Received GameStartReq, broadcasting GameStarting event to all clients.')
+      io.emit('GameStarting', 'wow sample text, this will send the amount of players and some other fun info later')
+      setTimeout(() => {
+        utils.log.socketio('timeout elapsed, sending first Question..')
+        io.emit("GameQuestion", {"question": "Bla bla bla, test", "answers": [{"id": 1, "answer": "bla bla svar 1"}, {"id": 2, "answer": "bla bla svar 2"}, {"id": 3, "answer": "bla bla svar 3"}, {"id": 4, "answer": "blas ba svar 4"}]})
+      }, 5000)
+    })
+});
 
 app.get('/', (req, res) => {
   res.send('API')
