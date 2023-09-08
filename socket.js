977b51e151fcbb31e52e9123848adc247eb155e3
kicki228 <118886828+kicki228@users.noreply.github.com>
2023-09-08 13:13:37 +0200

    test to socket.io requset

--- socket.js
+++ socket.js
@@ -10,10 +10,10 @@ function socket(socket) {
     });
     socket.on('GameStartReq', () => {
       utils.log.socketio('Received GameStartReq, broadcasting GameStarting event to all clients.')
-      socket.broadcast.emit('GameStarting', 'wow sample text, this will send the amount of players and some other fun info later')
+      socket.emit('GameStarting', 'wow sample text, this will send the amount of players and some other fun info later')
       setTimeout(() => {
         utils.log.socketio('timeout elapsed, sending first Question..')
-        socket.broadcast.emit("GameQuestion", {"question": "Bla bla bla, test", "answers": [{"id": 1, "answer": "bla bla svar 1"}, {"id": 2, "answer": "bla bla svar 2"}, {"id": 3, "answer": "bla bla svar 3"}, {"id": 4, "answer": "blas ba svar 4"}]})
+        socket.emit("GameQuestion", {"question": "Bla bla bla, test", "answers": [{"id": 1, "answer": "bla bla svar 1"}, {"id": 2, "answer": "bla bla svar 2"}, {"id": 3, "answer": "bla bla svar 3"}, {"id": 4, "answer": "blas ba svar 4"}]})
       }, 5000)
     })
   }
