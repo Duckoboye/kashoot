7ea2433364591d6972a820845ea370173c847f63
Duckoboye <48358179+Duckoboye@users.noreply.github.com>
2023-10-12 08:56:24 +0200

    move function to utils

--- socket.js
+++ socket.js
@@ -32,20 +32,12 @@ function initializeSocket(server) {
         
         socket.on('GameAnswer', (e) => {
             if (gameState != 'started') return;
-
-            function registerAnswer(userid, answerid) { //adds answers to the 'answers' Set and prevents duplicates.
-                const set = JSON.stringify({userid, answerid}) //Gotta do this since Sets compare by reference, not content. This was fun to debug.
-
-                //Check uniqueness, warn if not.
-                if (!answers.has(set)) answers.add(set)
-                return utils.warn.socketio(`${socket.id} attempted to answer, but has already answered this question.`)
-            }
             
             utils.log.socketio(`Recieved answer ${e} from ${socket.id}`)
-            registerAnswer(socket.id, e)
+            utils.socket.registerAnswer(answers, socket.id, e)
             
             if (answers.count >= clients.count) {
-                utils.log.socketio('answers >= connectedClients')
+                utils.log.socketio('All clients have now answered!')
                 io.emit('GameResult', 1)
             }
         })
