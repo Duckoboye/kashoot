f8a21b119ceadaed46c9ea7559f34f7c5d159b61
Duckoboye <48358179+Duckoboye@users.noreply.github.com>
2023-11-07 14:28:13 +0100

    refactor socketClient

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ export const socketClientLogger = new Logger('socketio-client')
 export const serialLogger = new Logger('serial')
 
 export const io = createSocketServer(httpServer);
-export const ioc = createSocketClient()
+export const ioc = createSocketClient('http://localhost:5000')
 export const serial = createSerialServer('COM7')
 httpServer.listen(config.port, () => {
     expressLogger.log(`API server listening on http://127.0.0.1:${config.port}`);
