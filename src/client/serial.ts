9bf87133751d9e29aca9d0b3981be75439a25e7b
Duckoboye <48358179+Duckoboye@users.noreply.github.com>
2023-11-09 08:57:04 +0100

    refactor serial.ts

--- src/client/serial.ts
+++ src/client/serial.ts
@@ -1,6 +1,7 @@
 import {SerialPort} from 'serialport';
 import {ReadlineParser} from '@serialport/parser-readline';
 import { ioc, serialLogger } from '..';
+import { createSocketClient } from './socket-client';
 class Button {
   private id: number;
   private label: string;
@@ -19,11 +20,12 @@ class Button {
     return this.label
   }
 
-  setValue(newValue: number): void {
-    this.value = (newValue === 1?true:false)
+  setValue(newValue: boolean): void {
+    this.value = newValue
   }
 }
 
+//This map contains all the different input ids and their corresponding commands.
 const ids = new Map()
 ids.set('1', new Button(1,"Blue"))
 ids.set('2', new Button(2,"Red"))
@@ -34,28 +36,34 @@ ids.set('7', new Button(7, "Start"))
 export function createSerialServer(serialPort: string) {
   const port = new SerialPort({ path:serialPort, baudRate: 115200 });
   const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
-
+  let ioc;
   port.on('open', () => {
     serialLogger.log(`Serial port ${serialPort} is open.`);
+    ioc = createSocketClient('http://localhost:5000')
   });
   parser.on('data', (line: string) => {
-    parseData(line)
+    if (line.length === 0) return serialLogger.warn("Received empty data.");
+    const [ firstChar, value ] = parseData(line)
+
+    const component = ids.get(firstChar)
+    if (component instanceof Button) {
+    component.setValue(value==='1') //Verify whether this works or not
+    const buttonLabel = component.getLabel()
+    const buttonValue = component.getValue()
+    if (buttonLabel == 'Start' && buttonValue == true) {
+      serialLogger.log('Start button pressed!')
+      }
+    }
+    
   });
   port.on('error', (err: Error) => {
     serialLogger.error('Error: '+ err.message);
   });
 }
 
-function parseData(data: string) {
-  if (data.length === 0) return serialLogger.warn("Received empty data.");
-  const firstChar = data.charAt(0);
-  const value = Number(data.slice(1))
-  const button = ids.get(firstChar)
-  button.setValue(value)
-  const buttonLabel = button.getLabel()
-  const buttonValue = button.getValue()
-  serialLogger.log(`${buttonLabel} | ${buttonValue}`)
-  if (buttonLabel == 'Start' && buttonValue == true) {
-    serialLogger.log('Start button pressed!')
-  }
+function parseData(data: string): [string, string] {
+  const firstChar = data[0];
+  const value = data.slice(1)
+  serialLogger.log(`Received data: ${firstChar}:${value}`)
+  return [firstChar, value]
 }
