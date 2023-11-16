a6255b28f203abe2eb13668917b8cd6516a5c1e8
Duckoboye <48358179+Duckoboye@users.noreply.github.com>
2023-11-16 09:53:40 +0100

    refactor serial.ts

--- src/client/serial.ts
+++ src/client/serial.ts
@@ -1,5 +1,5 @@
-import {SerialPort} from 'serialport';
-import {ReadlineParser} from '@serialport/parser-readline';
+import { SerialPort } from 'serialport';
+import { ReadlineParser } from '@serialport/parser-readline';
 import { serialLogger } from '..';
 import { createSocketClient } from './socket-client';
 import { type Socket } from 'socket.io-client';
@@ -28,49 +28,58 @@ class Button {
 
 //This map contains all the different input ids and their corresponding commands.
 const ids = new Map()
-ids.set('1', new Button(1,"Blue"))
-ids.set('2', new Button(2,"Red"))
-ids.set('3', new Button(3,"Yellow"))
-ids.set('4', new Button(4,"Green"))
+ids.set('1', new Button(1, "Blue"))
+ids.set('2', new Button(2, "Red"))
+ids.set('3', new Button(3, "Yellow"))
+ids.set('4', new Button(4, "Green"))
 ids.set('7', new Button(7, "Start"))
 
 export function createSerialServer(serialPort: string) {
-  const port = new SerialPort({ path:serialPort, baudRate: 115200 });
+  const port = new SerialPort({ path: serialPort, baudRate: 115200 });
   const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
   let socket: Socket;
-  port.on('open', () => {
-    serialLogger.log(`Serial port ${serialPort} is open.`);
-    socket = createSocketClient('http://localhost:5000', port)
-    socket.emit('joinGame','bla123') //This should be moved into its own input later on, but this is fine for the MVP.
-  });
-  parser.on('data', (line: string) => {
-    if (line.length === 0) return serialLogger.warn("Received empty data.");
-    const [ firstChar, value ] = parseData(line)
-    const state = Number(value)
-    const component = ids.get(firstChar)
-    if (component instanceof Button) {
-    component.setValue(!!state) //Verify whether this works or not
-    const buttonLabel = component.getLabel()
-    const buttonValue = component.getValue()
-    if (buttonValue) {
-      serialLogger.log(buttonLabel+' button pressed!')
-      if (buttonLabel === 'Start') socket.emit('GameStartReq')
-      else socket.emit('GameAnswer', buttonLabel)
+
+  function handleDataReceived(line: string) {
+    if (line.length === 0) {
+      serialLogger.warn("Received empty data.");
+      return;
     }
-    if (buttonLabel == 'Start' && buttonValue) {
-      
+    const [firstChar, value] = parseData(line);
+    const state = Number(value);
+    const component = ids.get(firstChar);
+    if (component instanceof Button) {
+      component.setValue(!!state);
+
+      const buttonLabel = component.getLabel();
+      const buttonValue = component.getValue();
+
+      if (buttonValue) {
+        serialLogger.log(buttonLabel + ' button pressed!');
+        if (buttonLabel === 'Start') socket.emit('GameStartReq');
+        else socket.emit('GameAnswer', buttonLabel);
       }
     }
-    
-  });
+  }
+
+  function handlePortOpen() {
+    serialLogger.log(`Serial port ${serialPort} is open.`);
+    socket = createSocketClient('http://localhost:5000', port); //should be configurable or automatically assigned in the future.
+    socket.emit('joinGame', 'bla123');
+  }
+
+  port.on('open', handlePortOpen);
+
+  parser.on('data', handleDataReceived);
+
   port.on('error', (err: Error) => {
-    serialLogger.error('Error: '+ err.message);
+    serialLogger.error('Error: ' + err.message);
   });
 }
 
 function parseData(data: string): [string, string] {
   const firstChar = data[0];
-  const value = data.slice(1)
-  serialLogger.log(`Received data: ${firstChar}:${value}`)
-  return [firstChar, value]
+  const value = data.slice(1);
+  serialLogger.log(`Received data: ${firstChar}:${value}`);
+  return [firstChar, value];
 }
+
