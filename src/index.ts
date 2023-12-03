05d4cd25a08dfa40158174822eb6d0dc324b88b4
Duckoboye <48358179+Duckoboye@users.noreply.github.com>
2023-12-03 22:42:08 +0100

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import 'dotenv/config'
-import { createSerialPort, createSerialServer } from './client/serial';
+import { createSerialPort } from './client/serial';
 import {config} from './utils/utils'
 import { createExpressServer } from './server/server';
 import { createSocketServer } from './server/socket';
-import KashootClient from './client/KashootClient';
+import KashootController from './client/KashootController';
 
-if (process.env.SERIALPORT) {
-  createSerialServer(createSerialPort(process.env.SERIALPORT))
-}
+ if (process.env.SERIALPORT) {
+   const port = createSerialPort(process.env.SERIALPORT)
+   new KashootController(port, "http://localhost:5000")
+ }
+
 const httpServer = createExpressServer(config.port)
 export const io = createSocketServer(httpServer);
-
-const test = new KashootClient('http://localhost:5000', 'testclient')
